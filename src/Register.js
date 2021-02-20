496fab63c8136aed56f84649ff4b6ab9611824c2
root <dcdev001@gmail.com>
2021-02-20 14:31:50 +0000

    Add Contents

--- src/Register.js
+++ src/Register.js
@@ -5,6 +5,15 @@ class Register extends Component {
 	render() {
 		return (
 			<div className="register">
+				<div className="container">
+					<div>
+						Your Learning Journey Matters to Us
+					</div>
+					<hr />
+					<div>
+						Future achievers study from IITians Online
+					</div>
+				</div>
 			</div>
 		)
 	}
